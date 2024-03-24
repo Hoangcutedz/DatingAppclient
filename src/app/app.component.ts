6582d104bbec692cb5379bc36e8b8d596b9c96d4
Hoangcutedz <hoangn2422@gmail.com>
2024-03-24 15:40:16 +0700

    end of section 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  title = 'Dating app';
+  users: any;
+
+  constructor(private http: HttpClient) {}
+  
+  ngOnInit(): void {
+    // send a http get request to https://localhost:5001/api/users
+    this.http.get('https://localhost:5001/api/users').subscribe({ // subscribe to listen events next, error, complete
+      next: response => this.users = response,
+      error: error => console.log(error),
+      complete: () => console.log('Request has completed')
+    })
+  }
 }
